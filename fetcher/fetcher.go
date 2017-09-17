299c68088e258b7d6ad3715483886d8dc49c439e
Aagat Adhikari <hi@aagat.com>
2017-09-18 00:07:04 +0545

    added basic fetcher

---
+++ fetcher/fetcher.go
@@ -0,0 +1,42 @@
+package fetcher
+
+import (
+	m "github.com/keighl/metabolize"
+	"log"
+	"net/http"
+)
+
+type PageInfo struct {
+	Title       string `meta:"og:title,title"`
+	Description string `meta:"og:description,description"`
+	Keywords    string `meta:"keywords"`
+	Type        string `meta:"og:type"`
+}
+
+func Boot(num int, jobs <-chan string, results chan<- string) {
+	for w := 1; w <= num; w++ {
+		go Worker(jobs, results)
+	}
+}
+
+func Worker(jobs <-chan string, result chan<- string) {
+	for url := range jobs {
+		log.Println(url)
+		resp, err := http.Get(url)
+		if err != nil {
+			panic(err)
+		}
+		defer resp.Body.Close()
+
+		metadata := new(PageInfo)
+
+		err = m.Metabolize(resp.Body, metadata)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		result <- url
+
+		log.Printf("%+v", metadata)
+	}
+}
