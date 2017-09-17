40e700bbcc0ca3be0158fbb3a28a339ae830c0a8
Aagat Adhikari <hi@aagat.com>
2017-09-17 18:28:19 +0545

    cleanup index logic

--- search/index.go
+++ search/index.go
@@ -2,26 +2,24 @@ package search
 
 import (
 	"github.com/blevesearch/bleve"
-	"log"
 )
 
 type Index struct {
-	Index bleve.Index
+	index bleve.Index
 }
 
 func OpenIndex(path string) (*Index, error) {
 
 	mapping := buildMapping()
-	var err error
 	index, err := bleve.New(path, mapping)
 
 	if err != bleve.ErrorIndexPathExists && err != nil {
 		return nil, err
 	}
 
-	return &Index{Index: index}, nil
+	return &Index{index: index}, nil
 }
 
 func (i *Index) Add(key string, val interface{}) {
-	i.Index.Index(key, val)
+	i.index.Index(key, val)
 }
