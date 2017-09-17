a993ba364ebdf2bf95bbb7a0fbedca7a353e2c03
Aagat Adhikari <hi@aagat.com>
2017-09-17 11:38:56 +0545

    added database tables

--- main.go
+++ main.go
@@ -11,13 +11,14 @@ import (
 type Tag string
 
 type Bookmark struct {
+	Id           int       `json:"id"`
 	Created      time.Time `json:"created"`
 	LastUpdated  time.Time `json:"last_updated"`
 	LastVerified time.Time `json:"last_verified"`
 	Title        string    `json:"title"`
 	Description  string    `json:"description"`
 	Url          string    `json:"url"`
-	UrlHash      string    `json:"id"`
+	Hash         string    `json:"hash"`
 	Tags         []Tag     `json:"tags"`
 	Alive        bool      `json:"alive"`
 	Archived     bool      `json:"archived"`
@@ -25,12 +26,40 @@ type Bookmark struct {
 
 func main() {
 
-	db, err := sql.Open("sqlite3", ":memory")
+	db, err := sql.Open("sqlite3", ":memory:")
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	defer db.Close()
+
+	bootstrapTable := `
+CREATE TABLE IF NOT EXISTS bookmarks (
+  id INTEGER PRIMARY KEY,
+  created DATETIME,
+  updated DATETIME,
+  verified DATETIME,
+  title TEXT,
+  description TEXT,
+  url TEXT,
+  hash TEXT,
+  tags TEXT,
+  alive TINYINT,
+  archived TINYINT
+);
+
+CREATE TABLE IF NOT EXISTS tags (
+  id INTEGER PRIMARY KEY,
+  bookmark_id INTEGER,
+  tag TEXT
+);
+
+CREATE INDEX IF NOT EXISTS urlhash ON bookmarks (hash);
+`
+	_, err = db.Exec(bootstrapTable)
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("Hello world"))
